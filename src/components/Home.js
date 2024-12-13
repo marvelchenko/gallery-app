35b66643401a32d1ece53b0c41ea8111fb3ecac0
Egor Marvelous <marvelchenko@gmail.com>
2024-12-13 09:33:58 +0100

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div class="p-8 bg-white shadow-md fixed w-full">
+  <h1 class="text-6xl font-extrabold leading-none text-center bg-gradient-to-r from-red-800 via-yellow-500 to-yellow-500 bg-clip-text p-4 text-transparent">
+  Photo Gallery
+</h1>
+</div>
+  )
+}
+
+export default Home
